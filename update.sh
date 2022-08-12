ea3e0749d56f5e5a29fac6774410abb09d34c0d4
James Gagnon <sainted@gmail.com>
2022-08-12 13:16:31 -0600

    updated

--- update.sh
+++ update.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-echo -e "Deleting GIT repo\n"
-rm -rf pms-docker
+#echo -e "Deleting GIT repo\n"
+#rm -rf pms-docker
 
-echo -e "Cloning Plex Github repo\n"
-git clone https://github.com/plexinc/pms-docker.git
+#echo -e "Cloning Plex Github repo\n"
+#git clone https://github.com/plexinc/pms-docker.git
 
-echo -e "Copying my patch file to GIT repo\n"
-cp Dockerfile.patch pms-docker/
+#echo -e "Copying my patch file to GIT repo\n"
+#cp Dockerfile.patch pms-docker/
 cd pms-docker
 
-echo -e "Applying my patch file to GIT repo\n"
-git apply Dockerfile.patch
+#echo -e "Applying my patch file to GIT repo\n"
+# created with cd pms-docker && git diff > ../Dockerfile.patch
+#git apply ../Dockerfile.patch
 
 echo -e "Building Plex container image\n"
-docker build -t pms-docker-nvidia:mine .
+# Rename old image encase I need to restore
+docker tag pms-docker-nvidia:mine pms-docker-nvidia:$(date +%s)
+#docker build --no-cache --tag pms-docker-nvidia:mine .
 cd ..
 
 echo -e "Running Docker compose"
-#docker pull plexinc/pms-docker
-docker-compose -f plex.compose stop
+docker-compose -f plex.compose down
+docker-compose -f plex.compose build --no-cache --pull
 docker-compose -f plex.compose up --detach
